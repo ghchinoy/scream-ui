3d97595a83d8bd2cb6112d9a03e4fa25c47a58df
Hussain Chinoy <ghchinoy@google.com>
2026-02-13 09:15:33 -0700

    fix(ci): separate library build from demo build to resolve github pages 404 error

--- packages/lit-audio-ui/vite.config.ts
+++ packages/lit-audio-ui/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/scream-ui/',
   build: {
     outDir: 'dist',
     lib: {
---
+++ packages/lit-audio-ui/vite.demo.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Base public path for GitHub Pages
+  base: '/scream-ui/',
+  build: {
+    outDir: 'dist-demo',
+  }
+});
