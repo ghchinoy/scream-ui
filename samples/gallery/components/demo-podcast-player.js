1567192b81f0ad77af7871ab5b2e057e16c1df47
G. H. Chinoy <ghchinoy@gmail.com>
2026-02-20 17:07:50 -0700

    build: finalize workspace builds for a2a-showcase

--- samples/gallery/components/demo-podcast-player.js
+++ samples/gallery/components/demo-podcast-player.js
@@ -137,6 +137,7 @@ let DemoPodcastPlayer = class DemoPodcastPlayer extends LitElement {
     `;
     }
     async firstUpdated() {
+        // @ts-ignore - Ignore missing declaration file for dynamic import
         await import('@ghchinoy/lit-audio-ui/molecules/ui-orb.js');
     }
     _handleState(e) {
