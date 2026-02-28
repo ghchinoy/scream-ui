8a29e5733b2d948e7641d52b678dfb02c5db0e6b
G. H. Chinoy <ghchinoy@gmail.com>
2026-02-28 09:46:57 -0700

    chore: clean up stray build artifacts from source directories

--- packages/lit-audio-ui/src/utils/audio-context.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from '@lit/context';
-/**
- * A unique token to identify our context.
- * Any component that uses @consume({context: audioPlayerContext})
- * will automatically receive updates when the nearest <ui-audio-provider> changes its state.
- */
-export const audioPlayerContext = createContext('ui-audio-player-context');
-//# sourceMappingURL=audio-context.js.map
--- packages/lit-audio-ui/src/utils/speech-context.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from '@lit/context';
-export const speechContext = createContext('ui-speech-context');
-//# sourceMappingURL=speech-context.js.map
