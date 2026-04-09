83117675357431c614d5e3cafc29f8c7cf5d3a8e
G. H. Chinoy <ghchinoy@gmail.com>
2026-04-09 09:46:04 -0600

    chore(release): publish v0.2.6 for lit-text-ui (fix stale build output)

--- packages/lit-text-ui/src/index.js
+++
@@ -1,6 +0,0 @@
-// --- ATOMS ---
-export * from './components/atoms/ui-synced-transcript.js';
-// --- ORGANISMS (Composite Components) ---
-export * from './components/organisms/ui-audio-tag-editor.js';
-export * from './components/organisms/ui-chat-log.js';
-//# sourceMappingURL=index.js.map
